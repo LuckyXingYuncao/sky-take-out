bb13336452a3ba285ba7eeccbf84466786a1345d
LuckyXingYuncao <131509105+LuckyXingYuncao@users.noreply.github.com>
2026-09-10 20:48:05 +0800

    构建UserMapper.xml并更新UserMapper接口

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -1,10 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.User;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
@@ -14,15 +11,24 @@ public interface UserMapper {
      * @param openid
      * @return
      */
-    @Select("select * from user where openid = #{openid}")
     User getByOpenid(String openid);
 
     /**
      * 插入用户
      * @param user
      */
-    @Insert("insert into user (openid, name, phone, sex, id_number, avatar, create_time) " +
-            "values (#{openid}, #{name}, #{phone}, #{sex}, #{idNumber}, #{avatar}, #{createTime})")
-    @Options(useGeneratedKeys = true, keyProperty = "id")
     void insert(User user);
+
+    /**
+     * 根据ID查询用户
+     * @param id
+     * @return
+     */
+    User getById(Long id);
+
+    /**
+     * 更新用户信息
+     * @param user
+     */
+    void update(User user);
 }
